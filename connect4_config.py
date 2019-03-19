9d5a69785f962290769dc7500f44722771cc7c78
alexanderwangus <aswang@stanford.edu>
2019-03-18 17:34:22 -0700

    added connect4 neural net to run.py

---
+++ connect4_config.py
@@ -0,0 +1,73 @@
+"""
+config.py
+"""
+import os
+from multiprocessing import cpu_count
+import torch
+from functools import partial
+
+from games.connect4.Connect4Game import Connect4Game
+from games.connect4.Connect4NNet import Connect4NNet
+# from games.tictactoe.TicTacToeGame import TicTacToeGame
+# from games.tictactoe.TicTacToeNNet import TicTacToeNNet
+from players.RandomPlayer import RandomPlayer
+from players.OneStepLookaheadPlayer import OneStepLookaheadPlayer
+
+
+class Config():
+    # Overall setting
+    name = 'alpha234_connect4'
+    game = Connect4Game
+    nnet = Connect4NNet
+    use_multiprocessing = False
+
+
+    # RL Training
+    numIters = 1000
+    numEps = 100
+    tempThreshold = 15
+    updateThreshold = 0.6
+    maxlenOfQueue = 200000
+    numMCTSSims = 25
+    arenaCompare = 40 # number of games of self play to choose previous or current nnet
+    cpuct = 1
+    numItersForTrainExamplesHistory = 20
+
+
+    # Hardware
+    num_workers = cpu_count()
+    cuda = torch.cuda.is_available() # use cuda if available
+
+
+    # Model Architecture
+    dropout = 0.3
+    num_channels = 512
+    nnet_kwargs = {'num_channels':num_channels, 'dropout':dropout}
+
+
+    # Model Training
+    epochs = 10 # number of epochs of train model given a single iteration
+    batch_size = 64
+    lr = 0.001
+    optimizer = torch.optim.Adam
+    optimizer_kwargs = {'betas': (0.9, 0.999)}
+    lr_scheduler = torch.optim.lr_scheduler.StepLR
+    lr_scheduler_kwargs = {'step_size':1, 'gamma':1}
+
+
+    # Metrics
+    metric_opponents = [RandomPlayer, OneStepLookaheadPlayer]
+    metricArenaCompare = 20 # number of games to play against metric opponent
+
+
+    # Model Loading
+    checkpoint = os.path.join('saved/', name)
+    load_model = False # load model
+    load_model_file = (checkpoint, 'best.pth.tar')
+    load_train_examples = False # load training examples
+    load_folder_file = (checkpoint,'best.pth.tar') # file to training examples
+
+
+    # Logging
+    log_dir = 'saved/runs'
+    tensorboardX = True
--- run.py
+++ run.py
@@ -11,16 +11,32 @@ from Arena import Arena, ArenaMP
 
 from games.connect4.Connect4Game import Connect4Game, display
 from games.connect4.Connect4Players import HumanConnect4Player
+from games.connect4.Connect4NNet import Connect4NNet
 from players.OneStepLookaheadPlayer import OneStepLookaheadPlayer
+from players.RandomPlayer import RandomPlayer
+from PytorchNNet import NNetWrapper as NNet
+from MCTS import MCTS
+from utils.util import *
+import torch
+from connect4_config import Config
+from players.NeuralNetPlayer import NNetPlayer
 
 def main():
     game = Connect4Game()
 
-    rp = OneStepLookaheadPlayer(game).play
-    rp2 = OneStepLookaheadPlayer(game).play
+    rp1 = RandomPlayer(game).play
+    osp1 = OneStepLookaheadPlayer(game).play
+    osp2 = OneStepLookaheadPlayer(game).play
     hp = HumanConnect4Player(game).play
-    # arena = Arena(hp, rp, game, display=display)
-    arena = ArenaMP(rp, rp2, game, display=display)
+    n1 = NNet(game, Config)
+    n1.load_checkpoint('./games/connect4/pretrained_models/','connect4_checkpoint_26.pth.tar')
+    mcts_args = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
+    n1p = NNetPlayer(game, n1, mcts_args).play
+
+    # mcts1 = MCTS(game, n1, mcts_args)
+    # n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+    arena = Arena(n1p, rp1, game, display=display)
+    # arena = ArenaMP(n1p, rp2, game, display=display)
 
     out = arena.playGames(10, verbose=True)
     print(out)
