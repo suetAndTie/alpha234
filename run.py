1968fdcccb16121c0a498bca43f69d5b720935b1
suetAndTie <trtets@gmail.com>
2019-03-20 20:02:24 -0700

    pushing weights and runs to github

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ from Arena import Arena, ArenaMP
 from games.connect4.Connect4Game import Connect4Game, display
 from games.connect4.Connect4Players import HumanConnect4Player
 from games.connect4.Connect4NNet import Connect4NNet
+from games.othello.OthelloGame import OthelloGame
 from players.OneStepLookaheadPlayer import OneStepLookaheadPlayer
 from players.MCTSPlayer import MCTSPlayer
 from players.NNetPlayer import NNetPlayer
@@ -20,19 +21,19 @@ from PytorchNNet import NNetWrapper
 from connect4_config import Config
 
 def main():
-    game = Connect4Game()
+    game = OthelloGame()
     config = Config()
 
     rp = RandomPlayer(game).play
     oslp = OneStepLookaheadPlayer(game).play
     hp = HumanConnect4Player(game).play
     mctsp = MCTSPlayer(game, config).play
-    nn = NNetWrapper(game, config)
-    ckpt = ('./weights/alpha234_connect4','connect4_best_34.pth.tar')
-    nn.load_checkpoint(ckpt[0], ckpt[1])
-    nnp = NNetPlayer(game, nn, config).play
+    # nn = NNetWrapper(game, config)
+    # ckpt = ('./weights/alpha234_connect4','connect4_best_34.pth.tar')
+    # nn.load_checkpoint(ckpt[0], ckpt[1])
+    # nnp = NNetPlayer(game, nn, config).play
 
-    arena = Arena(hp, nnp, game, display=display)
+    arena = Arena(hp, rp, game, display=display)
     # arena = ArenaMP(rp, rp2, game, display=display)
 
     out = arena.playGames(10, verbose=True)
